e36e0a6c491ae9defde4e4c468797868d26b1dae
SUSH9391 <masrbaliga@gmail.com>
2025-07-09 12:32:24 +0530

    added sign-in and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+// App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Hero from './components/Hero'; // Your homepage or parent component
+import Login from './components/Login';
 
 function App() {
-  
   return (
-    <>
-      <Navbar />
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
