80b2ef5b3f3088e4acde498f50a982033c289152
SUSH9391 <masrbaliga@gmail.com>
2025-07-09 13:53:59 +0530

    added ui/ux to login/signup

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -139,12 +139,14 @@ export default function Example() {
               thoughts, reflections, and experiences.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
+              <Link to="/login">
+                <a
+                  href="#"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get started
+                </a>
+              </Link>
               {/*<a href="#" className="text-sm/6 font-semibold text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
               </a>*/}
